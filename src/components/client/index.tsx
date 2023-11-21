b2218788eb74bc8803862901b2de74f90d1c3c8c
Chris Breen <breenie@users.noreply.github.com>
2023-11-21 19:46:28 +0000

    Swapped forecastId for forecast.

--- src/components/client/index.tsx
+++ src/components/client/index.tsx
@@ -4,15 +4,11 @@ import { useState, useEffect } from "react";
 
 export default function Tagline() {
   const weather = ["☀️", "☔️", "🌨"];
-  const [forecastId, setForecastId] = useState(0);
+  const [forecast, setForecast] = useState("");
 
   useEffect(() => {
-    setForecastId(Math.floor(Math.random() * weather.length));
+    setForecast(weather[Math.floor(Math.random() * weather.length)]);
   }, []);
 
-  return (
-    <>
-      Made in Sunny Lancashire {forecastId != null ? weather[forecastId] : ""}
-    </>
-  );
+  return <>Made in Sunny Lancashire {forecast}</>;
 }
